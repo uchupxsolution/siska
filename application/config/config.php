29c10d1833f90202e93054a885d368019d156ac9
Yusuf N. Mambrasar, S.Kom <yusuf_mambrasar@yahoo.com>
2016-01-06 01:47:09 +0900

    Config Codeigniter: base_url, sess_cookie_name

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://macbook.net/';
 
 /*
 |--------------------------------------------------------------------------
@@ -356,7 +356,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'siska_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
