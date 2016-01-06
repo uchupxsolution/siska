f6b311012d60ae7e9e1a95a3b044503af90c4ea5
Yusuf N. Mambrasar, S.Kom <yusuf_mambrasar@yahoo.com>
2016-01-07 06:58:26 +0900

    Add: Create Basic View and Search Data with Pagination for user table

---
+++ application/views/user/list.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+    @project : SISKA
+    @date    : Jan 6, 2016, 11:51:58 AM
+    @author  : Yusuf N. Mambrasar, S.Kom
+    @email   : yusuf_mambrasar@yahoo.com
+    @company : CV. Uchupx Solution
+*/
+
+?>
+<html>
+    <title>User List</title>
+    <body>
+        <div class="contents">
+            <div class="nav">
+                <?php echo $link_create; ?>
+                <form action="<?php echo $action; ?>" method="post">
+                    <input type="text" name="search" value="<?php echo $search; ?>" />
+                    <input type="submit" value="Cari" name="submit_search" />
+                </form>
+            </div>
+            <div class="table">
+                <?php echo $table; ?>
+            </div>
+            <div class="navigation">
+                <?php echo $pagination; ?>
+            </div>
+        </div>
+    </body>
+</html>
