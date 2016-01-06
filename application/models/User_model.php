4f6a3631fa3a9de97fdc24e4e60ed2ef4a450702
Yusuf N. Mambrasar, S.Kom <yusuf_mambrasar@yahoo.com>
2016-01-07 06:55:20 +0900

    Modify: Add pin, salt, and password configuration

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -24,8 +24,19 @@ class User_model extends CI_Model
     private $table_name = 'user';
     
     /**
+     * PIN Length
+     * @var int 
+     */
+    private $pin_length = 10;
+    
+    /**
+     * Salt Length
+     * @var int 
+     */
+    private $salt_length = 10;
+    
+    /**
      * Constructor
-     * @return  void
      */
     function __construct() {
         parent::__construct();
@@ -37,38 +48,48 @@ class User_model extends CI_Model
      * @param int $offset
      * @param string $order_column
      * @param string $order_type
+     * @param string $search
      * @return array
      */
     function get_paged_list(
             $limit=10, 
             $offset=0, 
             $order_column='', 
-            $order_type='asc')
+            $order_type='asc',
+            $search='')
     {
-        if ( empty($order_column) || empty($order_type) )
+        if (empty($order_column)||empty($order_type))
             $this->db->order_by($this->primary_key,'asc');         
         else         
             $this->db->order_by($order_column, $order_type);
+        if(!empty($search))
+            $this->db->like('name',$search);
+        $this->db->where('status >=',0);
         return $this->db->get($this->table_name,$limit,$offset);
     }
     
     /**
      * Count all item on table
+     * @param string $search
      * @return int
      */
-    function count_all()
+    function count_all($search='')
     {
-        return $this->db->count_all($this->table_name);
+        if(!empty($search))
+            $this->db->like('name',$search);
+        $this->db->where('status >=',0);
+        $query=$this->db->get($this->table_name);
+        return $query->num_rows();
     }
     
     /**
      * Get item by id
-     * @param int $id
+     * @param int $user_id
      * @return array
      */
-    function get_by_id($id)
+    function get_by_id($user_id)
     {
-        $this->db->where($this->primary_key,$id);
+        $this->db->where($this->primary_key,$user_id);
         return $this->db->get($this->table_name);
     }
     
@@ -79,29 +100,86 @@ class User_model extends CI_Model
      */
     function insert($person)
     {
+        $person['salt']=$this->_get_salt_random();
+        $person['date_create']=$this->_get_date_now();
+        $person['date_update']=$this->_get_date_empty();
+        $person['date_delete']=$this->_get_date_empty();
         $this->db->insert($this->table_name, $person);
         return $this->db->insert_id();
     }
     
     /**
      * Update item on table
-     * @param id $id
+     * @param id $user_id
      * @param array $person
      */
-    function update($id,$person)
+    function update($user_id,$person)
     {
-        $this->db->where($this->primary_key,$id);
+        $person['salt']=$this->get_by_id($user_id)->row_array()['salt'];
+        if(!empty($person['password']))
+            $person['password']=$this->_format_password ($person);
+        $person['date_update']=$this->_get_date_now();
+        $this->db->where($this->primary_key,$user_id);
         $this->db->update($this->table_name,$person);
     }
     
     /**
-     * Delete item from table
-     * @param int $id
+     * Add item to trash
+     * @param int $user_id
      */
-    function delete($id)
+    function delete($user_id)
     {
-        $this->db->where($this->primary_key,$id);
-        $this->db->delete($this->table_name);
+        $person['status']=-1;
+        $person['date_delete']=$this->_get_date_now();
+        $this->db->where($this->primary_key,$user_id);
+        $this->db->update($this->table_name,$person);
+    }
+    
+/**
+     * Get Random PIN
+     * @return string
+     */
+    function _get_pin_random()
+    {
+        return substr(md5(date('HisYmd')),2,$this->pin_length);
+    }
+    
+    /**
+     * Get Random Salt
+     * @return string
+     */
+    function _get_salt_random()
+    {
+        return substr(md5(date('HYimsd')),5,$this->salt_length);
+    }
+    
+    /**
+     * Get date now from system
+     * @param string $format
+     * @return string
+     */
+    function _get_date_now($format='Y-m-d H:i:s')
+    {
+        return date($format);
+    }
+    
+    /**
+     * Get empty date
+     * @param string $format
+     * @return string
+     */
+    function _get_date_empty($format='Y-m-d H:i:s')
+    {
+        return date($format,  strtotime(0));
+    }
+    
+    /**
+     * 
+     * @param array $person
+     * @return string
+     */
+    function _format_password($person){
+        return md5($person['password'].$person['salt']);
     }
     
 }
