533b387c9d59c3a41103eaa241a37de681331b06
Yusuf N. Mambrasar, S.Kom <yusuf_mambrasar@yahoo.com>
2016-01-15 16:20:27 +0900

    Modify: Table name from projects_siska to projects_siska2

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://macbook.net/';
+$config['base_url'] = 'http://uchupx.net/';
 
 /*
 |--------------------------------------------------------------------------
